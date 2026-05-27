b3a7bfca40695a0797d70db957c1f2180a97d636
Hema281204 <hemathiyyagura123@gmail.com>
2026-05-27 11:10:29 +0530

    Fixed API base URL

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://picklestore-backend.onrender.com",
+    "https://picklestore-backend.onrender.com/api",
 });
 
 export default api;
